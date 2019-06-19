ceb711a5fe893a919fa76e59efce550a4c75ceda
林龙成 <linlc@192.168.0.119>
2019-06-20 03:47:26 +0800

    d

---
+++ HCHollyOC/HCHollyOC.h
@@ -0,0 +1,15 @@
+//
+//  HCHollyOC.h
+//  HCHollyOC
+//
+//  Created by 林龙成 on 2019/6/20.
+//  Copyright © 2019 loganv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HCHollyOC : NSObject
+
++(void)testA;
+
+@end
